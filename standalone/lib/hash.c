gap-packages/kbmag

commit e42c04d16356eb59d7c564664408c89bac080dea

Fix more warnings

standalone/lib/hash.c
@@ -644,6 +644,7 @@ hash_locate (hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     hash_morerecs(htptr);
+  ms=-1;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=hash_morespace(htptr); 
@@ -711,6 +712,7 @@ short_hash_locate (short_hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     short_hash_morerecs(htptr);
+  ms=-1;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=short_hash_morespace(htptr); 
@@ -778,6 +780,7 @@ char_hash_locate (char_hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     char_hash_morerecs(htptr);
+  ms=-1;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=char_hash_morespace(htptr); 
@@ -845,6 +848,7 @@ gen_hash_locate (gen_hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     gen_hash_morerecs(htptr);
+  ms=-1;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=gen_hash_morespace(htptr); 

standalone/lib/diffredwl.c
@@ -24,9 +24,7 @@ diff_reduce_wl (gen *w, reduction_struct *rs_wd)
  * the function make_substituion to make the actual substitution.
  */
 { int wordlen,  **watable,start,end,i,j,initstate, state;
-  fsa *wd_fsa = rs_wd->wd_fsa;
   fsa *wa = rs_wd->wa;
-  int *weight = rs_wd->weight;
   boolean reducible;
   w = w-1; /* since code below assumes word is from w[1] .. w[wordlen]. */
   watable = wa->table->table_data_ptr;
@@ -79,7 +77,6 @@ make_substitution (
       gct, *gpref, level, gen1, gen2, diff, diffct, newdiff,
       shift, cwordwt, csubwt, i, j;
   fsa *wd_fsa = rs_wd->wd_fsa;
-  fsa *wa = rs_wd->wa;
   int *weight = rs_wd->weight;
   int maxreducelen = rs_wd->maxreducelen;
   int maxv=MAXV;

standalone/lib/fsarevmid.c
@@ -65,7 +65,7 @@ fsa_reverse_short(fsaptr,op_table_type,destroy,subsets,tempfilename)
 	boolean subsets;
 	char *tempfilename;
 {
-  int **table, ne, nsi, ns, is, dr, *fsarow, e,
+  int **table, ne, nsi, ns, is, dr, *fsarow,
       nt, cstate, cs, csi, im, i, g, n, len, ct;
   unsigned short *ht_ptr, *ht_chptr, *ht_ptrb, *ht_ptre,
                  *cs_ptr, *cs_ptre, *ptr;
@@ -304,8 +304,8 @@ fsa_mireverse_short(fsaptr,op_table_type,destroy,tempfilename)
 	boolean destroy;
 	char *tempfilename;
 {
-  int **table, ne, nsi, ns, nai, is, dr, *fsarow, e,
-      nt, cstate, cs, csi, im, i, g, n, len, ct;
+  int **table, ne, nsi, ns, nai, is, dr, *fsarow,
+      nt, cstate, cs, csi, im, i, g, len, ct;
   unsigned short *ht_ptr, *ht_chptr, *ht_ptrb, *ht_ptre,
                  *cs_ptr, *cs_ptre, *ptr;
   boolean dense_ip, dense_op;
@@ -379,7 +379,6 @@ fsa_mireverse_short(fsaptr,op_table_type,destroy,tempfilename)
  * The initial states consists of all singleton accept states of *fsaptr.
  */
   short_hash_init(&ht,FALSE,0,0,0);
-  ht_ptre = ht_ptrb-1;
   for (i=1;i<=nai;i++) {
     ht_ptrb = ht.current_ptr;
     *ht_ptrb= nsi==nai ? i : fsaptr->accepting[i];
@@ -564,7 +563,7 @@ fsa_miexists1_short(fsaptr,op_table_type,destroy,tempfilename)
       nt, cstate, cs, csi, im, i, g1, len, ct, ni;
   unsigned short *ht_ptr, *ht_chptr, *ht_ptrb, *ht_ptre,
                  *cs_ptr, *cs_ptre, *ptr;
-  boolean dense_ip, dense_op, got;
+  boolean dense_ip, dense_op;
   short_hash_table ht;
   fsa *miexists1;
   FILE *tempfile, *fopen();
@@ -836,7 +835,7 @@ fsa_miexists2_short(fsaptr,op_table_type,destroy,tempfilename)
       nt, cstate, cs, csi, im, i, g2, len, ct, ni;
   unsigned short *ht_ptr, *ht_chptr, *ht_ptrb, *ht_ptre,
                  *cs_ptr, *cs_ptre, *ptr;
-  boolean dense_ip, dense_op, got;
+  boolean dense_ip, dense_op;
   short_hash_table ht;
   fsa *miexists2;
   FILE *tempfile, *fopen();

standalone/lib/kbfns.c
@@ -1443,7 +1443,6 @@ conf_check (rewriting_system *rwsptr)
     lb,
     r,
     iv,
-    rv,
     beta[MAXREDUCELEN],
     betar,
     y[MAXREDUCELEN],
@@ -1461,8 +1460,7 @@ conf_check (rewriting_system *rwsptr)
   int  **table = rwsptr->reduction_fsa->table->table_data_ptr;
   boolean discarded=FALSE,
     bt;
-  int *preflen = rwsptr->preflen,
-      *prefno = rwsptr->prefno;
+  int *preflen = rwsptr->preflen;
       reduction_struct rs_rws;
 
   rs_rws.rws= rwsptr;
@@ -1698,7 +1696,7 @@ type_sort_eqns (int x, rewriting_system *rwsptr)
  * first, then those involving only G generators, and finally those involving
  * only H generators.
  */
-{ int i, l, ct, ret;
+{ int i, ct, ret;
   gen **newlhs,
       **newrhs;
   boolean   *newdone;
@@ -1770,7 +1768,7 @@ type_sort_eqns_final (rewriting_system *rwsptr)
  * This is to sort out the different categories of equations for
  * the cosets enumeration.
  */
-{ int i, l, ct;
+{ int i, ct;
 
   gen **newlhs,
       **newrhs;

standalone/lib/nfa.c
@@ -1073,8 +1073,8 @@ nfa_exists_short(fsaptr,op_table_type,destroy,tempfilename)
 	boolean destroy;
 	char *tempfilename;
 {
-  int **table, ne, *tableptr, *tableptre, ngens, nsi, ns, dr, *fsarow, e,
-      es, ef, nt, cstate, cs, csi, im, i, k, g1, len, ct;
+  int **table, ne, *tableptr, *tableptre, ngens, nsi, ns, dr, *fsarow,
+      es, ef, nt, cstate, csi, im, i, k, g1, len, ct;
   unsigned short *ht_ptr, *ht_chptr, *ht_ptrb, *ht_ptre,
                  *cs_ptr, *cs_ptre, *ptr;
   boolean dense_ip, dense_op, got;
@@ -1334,8 +1334,8 @@ nfa_exists_int(fsaptr,op_table_type,destroy,tempfilename)
 	boolean destroy;
 	char *tempfilename;
 {
-  int **table, ne, *tableptr, *tableptre, ngens, nsi, ns, dr, *fsarow, e,
-      es, ef, nt, cstate, cs, csi, im, i, k, g1, len, ct;
+  int **table, ne, *tableptr, *tableptre, ngens, nsi, ns, dr, *fsarow,
+      es, ef, nt, cstate, csi, im, i, k, g1, len, ct;
   int *ht_ptr, *ht_chptr, *ht_ptrb, *ht_ptre,
                  *cs_ptr, *cs_ptre, *ptr;
   boolean dense_ip, dense_op, got;

standalone/lib/rabkar.c
@@ -307,7 +307,6 @@ slow_check_rws_reduce_rk(w,i,rwsptr)
       *histptr,
       *histptre,
       *nextptr,
-      longer,
       *slowhistorysp = rwsptr->slowhistorysp,
       **slowhistory = rwsptr->slowhistory;
   unsigned  bighashval,

standalone/lib/rwsio.c
@@ -601,7 +601,7 @@ print_kboutput (FILE *wfile, rewriting_system *rwsptr)
  * input, and is followed by a new field, the reduction automaton.
  * The fsa function fsa_print is used to print this.
  */
-{ int i, j, i1, l, n;
+{ int i, i1, n;
   boolean in,first;
 
   sprintf(kbm_buffer,"%s := rec(",rwsptr->name);

standalone/lib/rwsiob.c
@@ -363,7 +363,7 @@ read_eqns_simple(rfile,check,rwsptr)
  * simplified version of "read_eqns" in rwsio.c
  * Here we simply read them in and store them without any processing.
  */
-{ int delim, i, ct, l1, l2, max;
+{ int delim, ct, l1, l2, max;
   gen *test1 = rwsptr->testword1, *test2 = rwsptr->testword2;
   check_next_char(rfile,'[');
   read_delim(rfile,&delim);

commit f7282c26a0c46d2d984e37da097ee681dc8d71a1

Fix gpmakefsa
This fixes a regression introduced by me in e42c04d16356eb59d7c564664408c89bac080dea

standalone/lib/hash.c
@@ -604,7 +604,7 @@ hash_locate (hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     hash_morerecs(htptr);
-  ms=-1;
+  ms=0;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=hash_morespace(htptr); 
@@ -672,7 +672,7 @@ short_hash_locate (short_hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     short_hash_morerecs(htptr);
-  ms=-1;
+  ms=0;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=short_hash_morespace(htptr); 
@@ -740,7 +740,7 @@ char_hash_locate (char_hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     char_hash_morerecs(htptr);
-  ms=-1;
+  ms=0;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=char_hash_morespace(htptr); 
@@ -808,7 +808,7 @@ gen_hash_locate (gen_hash_table *htptr, int reclen)
 
   if (nr+2 >= htptr->maxrecs)
     gen_hash_morerecs(htptr);
-  ms=-1;
+  ms=0;
   if ((fixed && htptr->space_inc - htptr->block_space < reclen) ||
      (!fixed && htptr->space_inc - htptr->block_space <= HTMARGIN) )
     ms=gen_hash_morespace(htptr); 

standalone/src/gpmakefsa.c
@@ -71,6 +71,7 @@
  */
 
 #include <stdio.h>
+#include <assert.h>
 #include "defs.h"
 #include "fsa.h"
 #include "rws.h"
@@ -256,7 +257,8 @@ main (int argc, char *argv[])
     wa = cosets ?
       fsa_wa_cos(&diff2,op_store_wa,TRUE,tempfilename) :
       fsa_wa(&diff2,op_store_wa,TRUE,tempfilename);
-  
+    assert(wa);
+
     if (kbm_print_level>1)
       printf("  #Number of states of word-acceptor before minimisation = %d.\n",
           wa->states->size);